989083cc81bdb1db119cf37e58720dc1120cf1fd
Brad <bradkyleduncan@gmail.com>
2018-03-19 07:55:19 +0200

    added google cloud authentication to the build

--- .circleci/gcloud_setup.sh
+++ .circleci/gcloud_setup.sh
@@ -1,3 +1,5 @@
+apk --update add openjdk7-jre
+gcloud components install app-engine-java kubectl
 gcloud --quiet components update
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
