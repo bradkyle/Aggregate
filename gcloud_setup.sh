84dc71a8544478291d2d1f2bcf4d955d6090a99f
Brad <bradkyleduncan@gmail.com>
2018-03-19 08:40:20 +0200

    removed gcloud setup sh and moved config therin to circle config file

--- gcloud_setup.sh
+++
@@ -1,5 +0,0 @@
-gcloud --quiet components update
-gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
-gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
-gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials ${GOOGLE_CLUSTER_NAME}  
